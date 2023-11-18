f3f8ac6e76dd7e1da1dd2ccdd91286c592059ff9
off01 <j4n.meyer@gmail.com>
2023-11-18 16:29:15 +0100

    Uprava drobná

--- e2e-tests/diagnostic/activity.post.test.ts
+++ e2e-tests/diagnostic/activity.post.test.ts
@@ -1,10 +1,10 @@
 import { test, expect } from "@playwright/test"; // eslint-disable-line
 import { checkResponseStatus, validateJsonSchema } from "@helper/expectsAsserts";
-import { diagnosticL3, diagnosticL3InvalidwhsServiceId } from "@datafactory/diagnostic"; // eslint-disable-line
+import { diagnosticL3 } from "@datafactory/diagnostic"; // eslint-disable-line
 
-test.describe("diagnostika test L3", async () => {
-  test("odeslání requestu pro zjistění diagnostiky L3", async ({ request }) => {
-    const requestBody = await diagnosticL3();
+test.describe("Diagnostika WHS L3", async () => {
+  test("POST with valid ServiceId", async ({ request }) => {
+    const requestBody = await diagnosticL3("80016090");
 
     const response = await request.post("/activityAPI/activity", {
       data: requestBody,
@@ -18,8 +18,8 @@ test.describe("diagnostika test L3", async () => {
     await validateJsonSchema("POST_activity", "Diagnostics", body); // await validateJsonSchema("POST_activity", "Diagnostics", body, true); - true v případě kdy chci nové schema vytvořit
   });
 
-  /*   test("odeslání nevalidního requestu pro zjistění diagnostiky L3", async ({ request }) => {
-    const requestBody = await diagnosticL3InvalidwhsServiceId();
+  test("POST with invalid ServiceId", async ({ request }) => {
+    const requestBody = await diagnosticL3("354534");
 
     const response = await request.post("/activityAPI/activity", {
       data: requestBody,
@@ -30,6 +30,10 @@ test.describe("diagnostika test L3", async () => {
     const body = await response.json();
     //console.log(JSON.stringify(body, null, 2));
 
-    await validateJsonSchema("POST_activity", "Diagnostics", body, true);
-  }); */
+    await validateJsonSchema("POST_activity", "Diagnostics", body); // await validateJsonSchema("POST_activity", "Diagnostics", body, true); - true v případě kdy chci nové schema vytvořit
+    expect(body).toHaveProperty('description');
+    expect(body).toHaveProperty('errorCode');
+    expect(body.description[0].value).toBe('Related entity doesn’t exist');
+    expect(body.errorCode[0].value).toBe('240');
+  });
 });
--- lib/datafactory/diagnostic.ts
+++ lib/datafactory/diagnostic.ts
@@ -1,28 +1,13 @@
 import { incrementCounterDiagnosticId } from "@helper/counterHelper";
 
-export async function diagnosticL3() {
+export async function diagnosticL3(whsServiceId: string) {
   const idDiagnostic = incrementCounterDiagnosticId();
   const orderBody = {
     id: `D_${idDiagnostic}`,
     type: "Diagnostics",
     relatedEntity: [
       {
-        id: "80016090", // Na hulváta zadaný platný whsServiceId
-        name: "whsServiceId",
-      },
-    ],
-  };
-  return orderBody;
-}
-
-export async function diagnosticL3InvalidwhsServiceId() {
-  const idDiagnostic = incrementCounterDiagnosticId();
-  const orderBody = {
-    id: `D_${idDiagnostic}`,
-    type: "Diagnostics",
-    relatedEntity: [
-      {
-        id: "AA", // Neplatné whsServiceId
+        id: whsServiceId, // Na hulváta zadaný platný whsServiceId 80016090
         name: "whsServiceId",
       },
     ],
